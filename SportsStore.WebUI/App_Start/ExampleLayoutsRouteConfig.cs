106d60c74c23b241bbf33d5c81ab3e338261280c
Miguel Costa <miguelpintodacosta@gmail.com>
2013-10-11 09:46:24 +0100

    Primeiro commit

---
+++ SportsStore.WebUI/App_Start/ExampleLayoutsRouteConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using BootstrapMvcSample.Controllers;
+using NavigationRoutes;
+
+namespace BootstrapMvcSample
+{
+    public class ExampleLayoutsRouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapNavigationRoute<SportsStore.WebUI.Controllers.ProductController>("Automatic Scaffolding", c => c.List(null, 1));
+
+            //routes.MapNavigationRoute<ExampleLayoutsController>("Example Layouts", c => c.Starter())
+            //      .AddChildRoute<ExampleLayoutsController>("Marketing", c => c.Marketing())
+            //      .AddChildRoute<ExampleLayoutsController>("Fluid", c => c.Fluid())
+            //      .AddChildRoute<ExampleLayoutsController>("Sign In", c => c.SignIn())
+            //    ;
+        }
+    }
+}
